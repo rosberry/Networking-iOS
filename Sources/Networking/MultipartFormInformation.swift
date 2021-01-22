408816ea75ef8661e4cafbc32f7db8bb9d8989d8
Tublad <tublad92@gmail.com>
2021-01-22 10:53:44 +0600

    add property MultipartFormInformation

---
+++ Sources/Networking/MultipartFormInformation.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2020 Rosberry. All rights reserved.
+//
+
+import Foundation
+
+    let data: Data
+    let contentType: String
+
+    init(data: Data, contentType: String) {
+        self.data = data
+        self.contentType = contentType
+    }
+}
